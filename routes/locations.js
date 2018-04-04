db09a7012d60a1e125e3b103ec12fb642b87a21e
xLight <anjanasupun05@gmail.com>
2018-04-04 06:13:51 +0530

    added add routeo n locations

--- routes/locations.js
+++ routes/locations.js
@@ -1,4 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const Location = require('./../models/locations');
+
 router.add('/add',(req,res)=>{
-    //TODO Implement
+    let id = req.body.id;
+    let name = req.body.name;
+    let description = req.body.description;
+    let lng = req.body.longitude;
+    let lat = req.body.latitude;
+    let alt = req.body.altitude;
+    if (id&&name&&description&&lng&&lat&&alt){
+        new Location ({
+            id,
+            name,
+            description,
+            longitude:lng,
+            latitude:lng,
+            altitude:alt
+
+        }).save().then((createdUser)=>{
+            res.send(createdUser);
+        });
+    }
+    else {
+        res.send("Invalid data.")
+    }
     res.send("Works");
 });
+
